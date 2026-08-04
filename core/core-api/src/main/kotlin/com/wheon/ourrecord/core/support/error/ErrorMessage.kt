28a17df6c002d9dff1a929abaa6b1fcf714d7259
이희연 HeeYeon Lee <dgc394722@gmail.com>
2026-08-04 10:34:38 +0900

    feat: API 오류 응답에 오류 코드 추가
    - 오류 유형 이름을 code 필드로 제공하도록 응답 구조 확장

--- core/core-api/src/main/kotlin/com/wheon/ourrecord/core/support/error/ErrorMessage.kt
+++ core/core-api/src/main/kotlin/com/wheon/ourrecord/core/support/error/ErrorMessage.kt
@@ -1,9 +1,11 @@
 package com.wheon.ourrecord.core.support.error
 
 data class ErrorMessage private constructor(
+    val code: String,
     val message: String,
 ) {
     constructor(errorType: ErrorType) : this(
+        code = errorType.name,
         message = errorType.message,
     )
 }
